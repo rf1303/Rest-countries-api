00022cc34d7b6378c6c3b334e71324ab5cdd5237
Ramiro Fernandez <pulgaramiro@gmail.com>
2026-02-25 09:55:18 -0300

    Inicio 03

---
+++ src/useFetch/useFetch.js
@@ -0,0 +1,23 @@
+
+const urlMain = "https://restcountries.com/v3.1";
+
+const fetchData = async (filterByRegion) => {
+    try {
+        const response = await fetch(`${urlMain}${filterByRegion}`);
+        if (!response.ok) {
+           throw new Error(`Error ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error("API not renponse: ", error.message);
+        throw error;
+    }
+};
+
+export const getAllCountries = () => {
+    return fetchData("/all?fields=name,flags");
+}
+
+console.log('getAllCountries:', getAllCountries());
