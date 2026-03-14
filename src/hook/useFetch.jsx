f5a2119ba206d97ec51aca26647b5c5e5aca069e
Ramiro Fernandez <pulgaramiro@gmail.com>
2026-03-14 11:17:20 -0300

    rama refactorArchivos detalles 02

--- src/hook/useFetch.jsx
+++ src/hook/useFetch.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 
 export const useFetch = (fetchApi) => {
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
     useEffect(() => {
